177c80e2f719be22a27fd224d1c94d174ad2fb4d
itsuki <itsuki.enjyoy@gmail.com>
2024-10-22 12:20:42 +0900

    Initial Commit

---
+++ SiriDemo/SiriDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriDemoApp.swift
+//  SiriDemo
+//
+//  Created by Itsuki on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SiriDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
